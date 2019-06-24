6e91b86f85892b29bf7fbf1df3e27d46402d3d2a
@thorning_m <matthewthorning@gmail.com>
2019-06-24 22:14:28 +0100

    remove async

--- babel.config.js
+++ babel.config.js
@@ -21,7 +21,6 @@ const presets = [
 ]
 const plugins = [
     'transform-class-properties',
-    '@babel/plugin-transform-regenerator',
     '@babel/plugin-proposal-object-rest-spread',
     'babel-plugin-emotion',
     '@babel/plugin-syntax-dynamic-import',
--- src/index.js
+++ src/index.js
@@ -30,84 +30,85 @@ const Clap = class extends React.Component {
     }
 
     async componentDidMount() {
-        const mojs = await import('mo-js')
-        const tlDuration = 300
-        const triangleBurst = new mojs.Burst({
-            parent: '#clap',
-            radius: { 50: 95 },
-            count: 5,
-            angle: 30,
-            children: {
-                shape: 'polygon',
-                radius: { 6: 0 },
-                scale: 1,
-                stroke: 'rgba(211,84,0 ,0.5)',
-                strokeWidth: 2,
-                angle: 210,
-                delay: 30,
-                speed: 0.2,
-                easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+        import('mo-js').then(mojs => {
+            const tlDuration = 300
+            const triangleBurst = new mojs.Burst({
+                parent: '#clap',
+                radius: { 50: 95 },
+                count: 5,
+                angle: 30,
+                children: {
+                    shape: 'polygon',
+                    radius: { 6: 0 },
+                    scale: 1,
+                    stroke: 'rgba(211,84,0 ,0.5)',
+                    strokeWidth: 2,
+                    angle: 210,
+                    delay: 30,
+                    speed: 0.2,
+                    easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+                    duration: tlDuration,
+                },
+            })
+
+            const circleBurst = new mojs.Burst({
+                parent: '#clap',
+                radius: { 50: 75 },
+                angle: 25,
                 duration: tlDuration,
-            },
+                children: {
+                    shape: 'circle',
+                    fill: 'rgba(149,165,166 ,0.5)',
+                    delay: 30,
+                    speed: 0.2,
+                    radius: { 3: 0 },
+                    easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+                },
+            })
+
+            const countAnimation = new mojs.Html({
+                el: '#clap--count',
+                isShowStart: false,
+                isShowEnd: true,
+                y: { 0: -30 },
+                opacity: { 0: 1 },
+                duration: tlDuration,
+            }).then({
+                opacity: { 1: 0 },
+                y: -80,
+                delay: tlDuration / 2,
+            })
+
+            const opacityStart = this.props.count > 0 ? 1 : 0
+
+            const countTotalAnimation = new mojs.Html({
+                el: '#clap--count-total',
+                isShowStart: false,
+                isShowEnd: true,
+                opacity: { [opacityStart]: 1 },
+                delay: (3 * tlDuration) / 2,
+                duration: tlDuration,
+                y: { 0: -3 },
+            })
+
+            const scaleButton = new mojs.Html({
+                el: '#clap',
+                duration: tlDuration,
+                scale: { 1.3: 1 },
+                easing: mojs.easing.out,
+            })
+
+            const clap = document.getElementById('clap')
+            clap.style.transform = 'scale(1, 1)'
+            this.animationTimeline = new mojs.Timeline()
+            this.animationTimeline.add([
+                countAnimation,
+                countTotalAnimation,
+                scaleButton,
+                circleBurst,
+                triangleBurst,
+            ])
         })
-
-        const circleBurst = new mojs.Burst({
-            parent: '#clap',
-            radius: { 50: 75 },
-            angle: 25,
-            duration: tlDuration,
-            children: {
-                shape: 'circle',
-                fill: 'rgba(149,165,166 ,0.5)',
-                delay: 30,
-                speed: 0.2,
-                radius: { 3: 0 },
-                easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
-            },
-        })
-
-        const countAnimation = new mojs.Html({
-            el: '#clap--count',
-            isShowStart: false,
-            isShowEnd: true,
-            y: { 0: -30 },
-            opacity: { 0: 1 },
-            duration: tlDuration,
-        }).then({
-            opacity: { 1: 0 },
-            y: -80,
-            delay: tlDuration / 2,
-        })
-
-        const opacityStart = this.props.count > 0 ? 1 : 0
-
-        const countTotalAnimation = new mojs.Html({
-            el: '#clap--count-total',
-            isShowStart: false,
-            isShowEnd: true,
-            opacity: { [opacityStart]: 1 },
-            delay: (3 * tlDuration) / 2,
-            duration: tlDuration,
-            y: { 0: -3 },
-        })
-
-        const scaleButton = new mojs.Html({
-            el: '#clap',
-            duration: tlDuration,
-            scale: { 1.3: 1 },
-            easing: mojs.easing.out,
-        })
-
-        const clap = document.getElementById('clap')
-        clap.style.transform = 'scale(1, 1)'
-        this.animationTimeline = new mojs.Timeline()
-        this.animationTimeline.add([
-            countAnimation,
-            countTotalAnimation,
-            scaleButton,
-            circleBurst,
-            triangleBurst,
-        ])
     }
 
     getTheme() {
